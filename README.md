060fdc1e3ef117aef40cf14f8204910d2343ba3d
rafalkalita <rkalitainfo@gmail.com>
2016-10-20 23:30:11 +0100

    Add instructions how to dockerize and run. Added logging for the endpoint.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # rk-spring-boot-docker
 Simple spring boot application with Docker
 
+Build and run tests
 $ ./gradlew clean build
+
+Create docker image
 $ ./gradlew build buildDocker
+
+Run it!
+$ docker stop simple-app && docker rm simple-app
+$ docker run -e "SPRING_PROFILES_ACTIVE=prod" --name simple-app -p 8090:8080 -d -t com.rafalkalita/rk-spring-boot-docker
+$ docker logs simple-app -f
+
+Visit http://localhost:8090/ to test.
--- src/main/java/com/rafalkalita/app/Application.java
+++ src/main/java/com/rafalkalita/app/Application.java
@@ -1,5 +1,7 @@
 package com.rafalkalita.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    public final Logger logger = LoggerFactory.getLogger(Application.class);
+
     @RequestMapping("/")
     public String home() {
-        return "Rafal Kalita - This is spring-boot-docker java application hosted on AWS ec2";
+        logger.info("Endpoint has been hit!");
+        return "This is spring-boot-docker java application";
     }
 
     public static void main(String[] args) {
